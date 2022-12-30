352b41bfbd924704d053488364c17213bab6ba69
Alandis <1seals.alandis@gmail.com>
2022-12-30 10:23:24 -0600

    Added the sign in and reg view

---
+++ WhatsTheRecipe/Controllers/SignInController.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+class SignInController: UIViewController {
+    @IBOutlet weak var signinTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signInBtnPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func newUserBtnPressed(_ sender: UIButton) {
+    }
+    
+
+    @IBAction func forgotPasswordBtnPressed(_ sender: Any) {
+    }
+}
